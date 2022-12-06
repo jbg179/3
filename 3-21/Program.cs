839225c5fd3e4b4a0d4de436fe0f02076bc814ff
jbg179 <jbg179@naver.com>
2022-12-07 02:34:58 +0900

    3-21

---
+++ 3-21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+class ReadLineApp
+{
+    public static void Main()
+    {
+        int time, hour, minute, second;
+        Console.WriteLine("***Enter an integral time: ");
+        time = int.Parse(Console.ReadLine());
+        hour = time / 10000;
+        minute = time / 100 % 100;
+        second = time % 100;
+        Console.WriteLine("***Time is " +hour + ":" +minute + ":" +second);
+    }
+}
