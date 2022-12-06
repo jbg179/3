2be1d37c9645e2bb9cc7fe2b57be9be979740f7b
jbg179 <jbg179@naver.com>
2022-12-07 02:35:27 +0900

    3-21

--- 3-21/Program.cs
+++ 3-21/Program.cs
@@ -6,7 +6,7 @@ class ReadLineApp
     public static void Main()
     {
         int time, hour, minute, second;
-        Console.WriteLine("***Enter an integral time: ");
+        Console.Write("***Enter an integral time: ");
         time = int.Parse(Console.ReadLine());
         hour = time / 10000;
         minute = time / 100 % 100;
