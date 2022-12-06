7e0749a937f19fc16553e29f933f4267ce167c36
jbg179 <jbg179@naver.com>
2022-12-07 02:30:26 +0900

    3-19

---
+++ 3-19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+class OverflowApp
+{
+    public static void Main()
+    {
+        int i, max = int.MaxValue; // 정수 최대값 지정 (overflow 생성 용도)
+        try
+        {
+            Console.WriteLine("Start of try statement");
+            i = max + 1; // default: don’t check Overflow.
+            Console.WriteLine("After default overflow");
+            unchecked
+            {
+                i = max + 1; // don’t check Overflow intentionally.
+            }
+            Console.WriteLine("After unchecked statement");
+            checked
+            {
+                i = max + 1; // check Overflow
+            }
+            Console.WriteLine("After checked statement");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("caught an OverflowException");
+            }
+        }
+}
